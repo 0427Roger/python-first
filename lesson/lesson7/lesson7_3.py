f35b8569d9cb86d175ff63a9ba010cefae9318bc
羅韋翔 <160601895+0427Roger@users.noreply.github.com>
2024-03-18 10:47:38 +0800

    modifly

--- lesson/lesson7/lesson7_3.py
+++ lesson/lesson7/lesson7_3.py
@@ -1,24 +1,48 @@
 import random
-def play_game():
-  print('Guess the number game.')
+print('Guess the number game.')
+def main():
   while True:
     min=1
     max=100
+    count=0
     target=random.randint(1,100)
+    print(target)
     while True:
       try:
-        keyin:int=int(input(f'input namber(min~max):'))
+        count+=1
+        keyin:int=int(input(f'input namber({min}~{max}):'))
         if keyin<=min or keyin>=max:
           print('input range error.')
           continue
       except:
         print('input format error.')
         continue
-      if keyin<=target:
+      if keyin<target:
         print('Bigger')
         min=keyin
-      elif keyin>=target:
-        print('smallier')
+        continue
+      elif keyin>target:
+        print('Smallier')
         max=keyin
-      else keyin==target:
-        print('Guees')
+        continue
+      elif keyin==target:
+        print('Guessed correctly.')
+        print(f'Use {count} round.')
+        break
+    while True:
+      print('again(Y,N):')
+      is_again=input('again(Y,N):')
+      if is_again=='Y' or is_again=='N':
+        break
+      else:
+        print('input format error.')
+        continue
+    if is_again=='N':
+      print('Goodbye!')
+      break
+    elif is_again=='Y':
+      print('again')
+      continue
+if __name__=='__main__':
+  main()
+
---
+++ lesson/lesson7/lesson7_4.py
@@ -0,0 +1,49 @@
+import random
+print('Guess the number game.')
+def play_game():
+    min=1
+    max=100
+    count=0
+    target=random.randint(1,100)
+    print(target)
+    while True:
+      try:
+        count+=1
+        keyin:int=int(input(f'input namber({min}~{max}):'))
+        if keyin<=min or keyin>=max:
+          print('input range error.')
+          continue
+      except:
+        print('input format error.')
+        continue
+      if keyin<target:
+        print('Bigger')
+        min=keyin
+        continue
+      elif keyin>target:
+        print('Smallier')
+        max=keyin
+        continue
+      elif keyin==target:
+        print('Guessed correctly.')
+        print(f'Use {count} round.')
+        break
+def main():
+    while True:
+        play_game()
+        while True:
+            print('again(Y,N):')
+            is_again=input('again(Y,N):')
+            if is_again=='Y' or is_again=='N':
+                break
+            else:
+                print('input format error.')
+                continue
+        if is_again=='N':
+            print('Goodbye!')
+            break
+        elif is_again=='Y':
+            print('again')
+            continue
+if __name__=='__main__':
+    main()
