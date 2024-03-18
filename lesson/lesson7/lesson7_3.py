d15eed0e15dbeb403668aa00a002fde827e60531
羅韋翔 <160601895+0427Roger@users.noreply.github.com>
2024-03-18 09:28:38 +0800

    Update lesson7_3.py
    modifly

--- lesson/lesson7/lesson7_3.py
+++ lesson/lesson7/lesson7_3.py
@@ -1,1 +1,20 @@
-
+import random
+def play_game():
+  print('Guess the number game.')
+  while True:
+    min=1
+    max=100
+    target=random.randint(1,100)
+    while True:
+      try:
+        keyin=int(input(f'input namber(min~max):'))
+        if keyin<=min or keyin>=max:
+          print('input error.')
+          
+      except:
+        continue
+      if keyin<=target:
+        min=keyin
+      elif keyin>=target:
+        max
+    
