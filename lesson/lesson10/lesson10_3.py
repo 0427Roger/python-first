fc8c4cb3736c41c547dcb8ae16d6275ad238c7e1
羅韋翔 <160601895+0427Roger@users.noreply.github.com>
2024-03-29 10:17:52 +0000

    modifly

--- lesson/lesson10/lesson10_3.py
+++ lesson/lesson10/lesson10_3.py
@@ -1,7 +1,14 @@
 from tools import get_names,get_scores
 from pprint import pprint
 import pyinputplus
+import csv
 def main():
-    pprint(get_scores(get_names(pyinputplus.inputInt('input number value:',min=0,max=30))))
+    nums:int=pyinputplus.inputInt('input one number:',min=1,max=50)
+    names:list=get_names(nums)
+    scores:list[dict]=get_scores(names)
+    with(open('class.csv',mode='w',encoding='utf-8',newline=''))as(file):
+        dictwriter=csv.DictWriter(file,fieldnames=['name','nature','history','physics'])
+        dictwriter.writeheader()
+        dictwriter.writerows(scores)
 if __name__=='__main__':
     main()
