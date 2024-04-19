34b796488831463c7614c265daa504086ce57ed6
羅韋翔 <160601895+0427Roger@users.noreply.github.com>
2024-04-20 01:53:02 +0800

    modifly

---
+++ lesson11~20/lesson15/tools.py
@@ -0,0 +1,26 @@
+def get_names(num:int=5)->list[str]:
+    with open('names.txt',encoding='utf-8') as file:
+        context=file.read().split('\n')
+        names:str=random.choices(context,k=num)
+        return names
+
+def get_physical(names)->list[dict[str,int,int]]:
+    items:list=[]
+    for name in names:
+        item:dict={}
+        item['name']:str=name
+        item['height']:int=random.randint(150,200)
+        item['weight']:int=random.randint(50,80)
+        items.append(item)
+    return items
+
+def download(url)->response|None:
+    response:Response=requests.get(url)
+    try:
+        response.raise_for_status()
+        if response.status_code==200:
+            print('download successful')
+            return response
+    except Exception as e:
+        print(f'Error:{e}')
+        return None
