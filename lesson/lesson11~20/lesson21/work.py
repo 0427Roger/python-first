f18dd5f23ee7007ebd399c6cec8554ade064635b
羅韋翔 <160601895+0427Roger@users.noreply.github.com>
2024-05-13 06:48:35 +0000

    modifly

--- lesson/lesson11~20/lesson21/work.py
+++ lesson/lesson11~20/lesson21/work.py
@@ -10,10 +10,25 @@ def pathData_to_DataFrame(path):
     df1=pd.concat(pd.read_csv(path) for path in abspath)
     return df1
 
+def getData_json(url):
+    response=requests.get(url)
+    try:
+        response.raise_for_status()
+        if response.status_code==200:
+            data=response.json()
+            return data
+    except Exception as e:
+        print(f'ERROR{e}')         
+        return None
+url='https://ods.railway.gov.tw/tra-ods-web/ods/download/dataResource/0518b833e8964d53bfea3f7691aea0ee'
 path='Number of people entering and exiting each station every day'
 
 
 df1=pathData_to_DataFrame(path)
 df1['trnOpDate']=pd.to_datetime(df1['trnOpDate'].astype('str'))
-df1['trnOpDate']=df1.sort_values(by='trnOpDate')
+df1=(df1.sort_values(by='trnOpDate'))
+df1.groupby(by='staCode')
 print(df1)
+
+# data=getData_json(url)
+# pprint(data)
