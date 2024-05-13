beb71c8db230e194c6884689f5b5f8b88c81aa9c
羅韋翔 <160601895+0427Roger@users.noreply.github.com>
2024-05-13 13:03:39 +0000

    modifly

--- lesson/lesson11~20/lesson21/work.py
+++ lesson/lesson11~20/lesson21/work.py
@@ -20,15 +20,20 @@ def getData_json(url):
     except Exception as e:
         print(f'ERROR{e}')         
         return None
-url='https://ods.railway.gov.tw/tra-ods-web/ods/download/dataResource/0518b833e8964d53bfea3f7691aea0ee'
-path='Number of people entering and exiting each station every day'
-
-
-df1=pathData_to_DataFrame(path)
-df1['trnOpDate']=pd.to_datetime(df1['trnOpDate'].astype('str'))
-df1=(df1.sort_values(by='trnOpDate'))
-df1.groupby(by='staCode')
-print(df1)
-
-# data=getData_json(url)
-# pprint(data)
+def main():
+    url='https://ods.railway.gov.tw/tra-ods-web/ods/download/dataResource/0518b833e8964d53bfea3f7691aea0ee'
+    path='Number of people entering and exiting each station every day'
+    df1=pathData_to_DataFrame(path)
+    df1['trnOpDate']=pd.to_datetime(df1['trnOpDate'].astype('str'))
+    df1=(df1.sort_values(by='trnOpDate'))
+    df1=df1.rename(columns={'staCode':'stationCode'})
+    data=getData_json(url)
+    df2=pd.DataFrame(data,columns=['stationCode','stationName'])
+    df2['stationCode']=df2['stationCode'].astype('int')
+    df3=pd.merge(df1,df2,on='stationCode')
+    df4=(df3.groupby(by=['stationCode','stationName'])[['gateInComingCnt','gateOutGoingCnt']].agg(['sum','min','max']))
+    df4.columns.names=['person','statistics']
+    df5=df4.stack(level=['person','statistics'], future_stack=True).unstack(level=['stationCode','stationName'])
+    return df5
+if __name__=='__main__':
+    main()
